7f3bfdef45ea03f93b5834ba07e2b29144a928f2
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-22 19:51:06 -0500

    Various fixes

--- test/Soenneker.Utils.Strings.LevenshteinDistance.Tests/LevenshteinDistanceStringUtilTests.cs
+++ test/Soenneker.Utils.Strings.LevenshteinDistance.Tests/LevenshteinDistanceStringUtilTests.cs
@@ -12,18 +12,18 @@ public class LevenshteinDistanceStringUtilTests : HostedUnitTest
     {
     }
 
-    [Theory]
-    [InlineData("", "", 1)] // Two empty strings are identical
-    [InlineData("abc", "", 0)] // Completely different (non-overlapping) strings
-    [InlineData("", "xyz", 0)] // Completely different (non-overlapping) strings
-    [InlineData("kitten", "sitting", 0.5714)] // Levenshtein Distance = 3, max length = 7
-    [InlineData("kitten", "kitten", 1)] // Identical strings
-    [InlineData("abc", "def", 0)] // Completely different strings
-    [InlineData("abcdef", "abc", 0.5)] // Levenshtein Distance = 3, max length = 6
-    [InlineData("abc", "abcd", 0.75)] // Levenshtein Distance = 1, max length = 4
-    [InlineData("this is sitting on a porch", "this is sitting", 0.576)] // Adjusted based on edits
-    [InlineData("the cat sat on a hat", "sad on a hat", 0.55)] // Adjusted for deletions
-    [InlineData("this is a test", "this is another test", 0.7)] // Adjusted for substitutions and insertions
+    [Test]
+    [Arguments("", "", 1)] // Two empty strings are identical
+    [Arguments("abc", "", 0)] // Completely different (non-overlapping) strings
+    [Arguments("", "xyz", 0)] // Completely different (non-overlapping) strings
+    [Arguments("kitten", "sitting", 0.5714)] // Levenshtein Distance = 3, max length = 7
+    [Arguments("kitten", "kitten", 1)] // Identical strings
+    [Arguments("abc", "def", 0)] // Completely different strings
+    [Arguments("abcdef", "abc", 0.5)] // Levenshtein Distance = 3, max length = 6
+    [Arguments("abc", "abcd", 0.75)] // Levenshtein Distance = 1, max length = 4
+    [Arguments("this is sitting on a porch", "this is sitting", 0.576)] // Adjusted based on edits
+    [Arguments("the cat sat on a hat", "sad on a hat", 0.55)] // Adjusted for deletions
+    [Arguments("this is a test", "this is another test", 0.7)] // Adjusted for substitutions and insertions
     public void CalculateSimilarity_Returns_Correct_Similarity_Score(string str1, string str2, double expectedScore)
     {
         double similarityScore = LevenshteinDistanceStringUtil.Calculate(str1, str2);
@@ -31,3 +31,4 @@ public class LevenshteinDistanceStringUtilTests : HostedUnitTest
         similarityScore.Should().BeApproximately(expectedScore, 0.001);
     }
 }
+
